10898adc0fe04a603967331673e2520b34d1547a
Christian Fei <crifei93@gmail.com>
2017-10-22 01:58:01 +0200

    refactor test

--- create-subscription/test/integration.test.js
+++ create-subscription/test/integration.test.js
@@ -2,11 +2,13 @@ const {serial} = require('ava')
 const request = require('r2')
 const {deleteSubscription} = require('./_stripe-utils')
 
+const stripeEmail = 'integration-test@payments.cc'
 const testToken = 'tok_visa'
+const stripeToken = {id: testToken}
 
 serial('creates subscription for "monthly"', async t => {
-  const body = {stripeEmail: 'integration-test@payments.cc', stripeToken: {id: testToken}}
-  const response = await request.post('http://localhost:3000/', {json: body}).json
+  const json = {stripeEmail, stripeToken}
+  const response = await request.post('http://localhost:3000/', {json}).json
   t.true(response.hasOwnProperty('customer'))
   t.true(response.hasOwnProperty('subscription'))
   t.is(response.subscription.plan.name, 'monthly')
